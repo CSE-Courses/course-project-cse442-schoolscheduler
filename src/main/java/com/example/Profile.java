c316b42748189c90735d4cf05d665411dcfe3f22
KenZhou1 <56738542+KenZhou1@users.noreply.github.com>
2020-10-28 18:47:11 -0400

    Add files via upload
    Created a back button

---
+++ src/main/java/com/example/Profile.java
@@ -0,0 +1,46 @@
+package com.example.schoolscheduler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Profile extends AppCompatActivity {
+
+    public static final String EXTRA_TEXT = "com.example.application.example.EXTRA_TEXT";
+    public static final String EXTRA_NUMBER = "com.example.application.example.EXTRA_NUMBER";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        Button done = (Button)findViewById(R.id.done);
+        done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goBack();
+            }
+        });
+
+    }
+
+    public void goBack(){
+        //Grab the text file you want to update
+        EditText editName = (EditText) findViewById(R.id.editTextTextPersonName);
+        String name = editName.getText().toString();
+
+        //Pass the text as an integer for testing dont worry about this one
+        EditText editNumber = (EditText) findViewById(R.id.editTextPhone);
+        int number = Integer.parseInt(editNumber.getText().toString());
+
+
+        Intent done = new Intent(Profile.this,Setting.class);
+        done.putExtra(Intent.EXTRA_TEXT, name);
+        done.putExtra(Intent.EXTRA_PHONE_NUMBER, name);
+        startActivity(done);
+    }
+}
