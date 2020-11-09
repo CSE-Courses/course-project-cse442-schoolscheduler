5d1ce31f0731fd959e45607465ecd1adb8ee50b2
jennifer19971127 <ytsang@buffalo.edu>
2020-11-08 21:39:41 -0500

    Clean master

--- App.js
+++
@@ -1,9 +0,0 @@
-import React, { useState, useEffect } from 'react';
-import { View } from 'react-native';
-import SemesterList from './components/SemesterList';
-
-export default function App() {
-  return (
-      <SemesterList />
-  );
-}
